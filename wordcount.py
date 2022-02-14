b9b54ec99cac4862871e41b6631c2b090b8749e9
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-14 20:10:22 +0000

    Tested twain.txt

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,19 @@
-"""Count words in file."""
+def words_count(file_name):
+    """Count words in file."""
+    txt_file = open(file_name)
+    word_count = {}
 
-txt_file = open('test.txt')
-word_count = {}
+    for line in txt_file:
+        line = line.rstrip()
+        words = line.split(" ")
 
-for line in txt_file:
-    line = line.rstrip()
-    words = line.split(" ")
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
 
-    for word in words:
-        word_count[word] = word_count.get(word, 0) + 1
+    for word, number in word_count.items():
+        print(word, number)
+    # return word_count
 
-for word, number in word_count.items():
-    print(word, number)
+    txt_file.close()
 
-
-txt_file.close()
+words_count("twain.txt")
