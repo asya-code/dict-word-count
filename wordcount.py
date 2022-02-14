fcb7b93187a87abe5c1c4c83a297b8ebb535fe85
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-14 20:59:00 +0000

    Refactored code into 3 functions

--- wordcount.py
+++ wordcount.py
@@ -2,22 +2,33 @@
 import sys
 
 filename = sys.argv[1]
+def tokenize(line):
+    """Open file and generate list of each word"""
+    line = line.rstrip()
+    words = line.split(" ")
+    return words
+
+
 def words_count(filename):
     """Count words in file."""
     txt_file = open(filename)
-    word_count = {}
-
+    words_count = {}
     for line in txt_file:
-        line = line.rstrip()
-        words = line.split(" ")
+        words = tokenize(line)
 
         for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
-
-    for word, number in word_count.items():
-        print(word, number)
-    # return word_count
+            words_count[word] = words_count.get(word, 0) + 1
 
     txt_file.close()
+    return words_count
 
-words_count(filename)
+def print_words_count(filename):
+    """Print each key, value in dictionary"""
+
+    words_counts = words_count(filename)
+
+    for word, number in words_counts.items():
+        print(word, number)
+
+
+print_words_count(filename)
